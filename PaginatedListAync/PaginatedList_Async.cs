fe8ab05746594cd3fbc6545b1057739c917dd4c2
Sourav Das <sourav@imocha.io>
2023-12-21 02:25:27 +0530

    For Check

--- PaginatedListAync/PaginatedList_Async.cs
+++ PaginatedListAync/PaginatedList_Async.cs
@@ -11,6 +11,7 @@ namespace PaginatedListAync
     public class PaginatedList<T>
     {
         public List<T> Items { get; }
+
         public int PageNumber { get; }
         public int TotalPages { get; }
         public int TotalCount { get; }
